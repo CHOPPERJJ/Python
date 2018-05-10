1ecd9dbb021578ae5e1aa7c8628cff8e619a42b6
CHOPPER JJ <chopper_jj@qq.com>
2018-05-11 00:20:21 +0800

    11

--- LearningProject/Hanoi.py
+++ LearningProject/Hanoi.py
@@ -1,9 +1,11 @@
-def move (n, first, buffer, end):
+def move(n, first, buffer, end):
     if n == 1:
         print(first, '-->', end)
     else:
-        move (n-1, first, end, buffer )
-        move (1, first, buffer, end )
-        move (n-1, buffer, first, end)
+        move(n - 1, first, end, buffer)
+        move(1, first, buffer, end)
+        move(n - 1, buffer, first, end)
+
 
 move(3, 'A', 'B', 'C')
+
--- LearningProject/hello world.py
+++ LearningProject/hello world.py
@@ -2,7 +2,7 @@
 # 遍历set,set中为tuple的项目，x[0]访问第一项，x[1]访问第二项
 s = set([('Adam', 95), ('Lisa', 85), ('Bart', 59)])
 for x in s:
-    print x[0], ':', x[1]
+    print (x[0], ':', x[1])
 
 # 更新set或者删除set中的元素
 s = set(['Adam', 'Lisa', 'Paul'])
