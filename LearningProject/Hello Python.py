18688be6b63a4d7ec693d26ab0c3aae0f6459a77
CHOPPER JJ <chopper_jj@qq.com>
2018-05-15 18:13:45 +0800

    new python

--- LearningProject/Hello Python.py
+++ LearningProject/Hello Python.py
@@ -1,2 +1,40 @@
 # coding=utf-8
 # 进阶Python
+
+# 高阶函数
+import math
+
+
+def add(x, y, f):
+    return f(x) + f(y)
+
+
+print(add(25, 9, math.sqrt))
+
+
+# map()函数应用,capitalize()首字母大写的函数
+def format_name(s):
+    return s.capitalize()
+
+
+print(list(map(format_name, ['adam', 'LISA', 'barT'])))
+
+# 用reduce()函数求积
+from functools import reduce
+
+
+def prod(x, y):
+    return x * y
+
+
+print(reduce(prod, [2, 4, 5, 7, 12]))
+
+# filter()函数过滤出1-100中平方根是整数的数
+import math
+
+
+def is_sqr(x):
+    return math.sqrt(x) % 1 == 0
+
+
+print(list(filter(is_sqr, list(range(1, 101)))))
