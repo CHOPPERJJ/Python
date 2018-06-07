3069058c2f3defbec56b6071087746f613561b5b
CHOPPER JJ <chopper_jj@qq.com>
2018-06-07 18:14:18 +0800

    up

--- LearningProject/Object Oriented Programming.py
+++ LearningProject/Object Oriented Programming.py
@@ -451,3 +451,36 @@ s = Student('Bob', 'male', '59')
 s.name = 'Tim'
 s.score = 99
 print(s.name, s.score)
+
+
+# python中__call__()的运用方式
+class Fib(object):
+    def __init__(self, p_num):
+        self.num = p_num
+        a, b, L = 0, 1, []
+        for x in range(p_num):
+            L.append(a)
+            a, b = b, a + b
+        self.list = L
+
+    def __call__(self, p):
+        print('Fib is %s' % self.list)
+
+
+# 调用实例对象f，f即是函数也是对象，函数可以被调用，也就是直接调用实例对象函数
+f = Fib(10)
+print(f(10))
+
+
+class Fib(object):
+    def __call__(self, p_num):
+        self.num = p_num
+        a, b, L = 0, 1, []
+        for x in range(p_num):
+            L.append(a)
+            a, b = b, a + b
+        return L
+
+
+f = Fib()
+print(f(10))
--- LearningProject/test_code.py
+++ LearningProject/test_code.py
@@ -1,30 +1,23 @@
 class Fib(object):
-    def __init__(self, p_num):
-        self.num = p_num
-        a, b, L = 0, 1, []
-        for x in range(p_num):
-            L.append(a)
-            a, b = b, a + b
-        self.list = L
-
-    def __call__(self, p):
-        print('Fib is %s' % self.list)
-
-
-# 调用实例对象f，f即是函数也是对象，函数可以被调用，也就是直接调用实例对象函数
-f = Fib(10)
-print(f(10))
-
-
-class Fib(object):
-    def __call__(self, p_num):
-        self.num = p_num
-        a, b, L = 0, 1, []
-        for x in range(p_num):
-            L.append(a)
-            a, b = b, a + b
-        return L
+    def __getitem__(self, n):
+        if isinstance(n, int):
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice):
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop)
+                if x >= start:
+                    L.append(a)
+                    a, b = b, a + b
+            return L
 
 
 f = Fib()
-print(f(10))
+print(f[2])
