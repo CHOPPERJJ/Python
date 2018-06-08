4115b6e4562b4ff1f4c2e20dbf52fb02ee49bad5
CHOPPER JJ <chopper_jj@qq.com>
2018-06-08 18:17:21 +0800

    uopdate

--- LearningProject/test_code.py
+++ LearningProject/test_code.py
@@ -1,3 +1,4 @@
+# python的__getietm__()的特殊方法使用，让Fib表现的像list一样
 class Fib(object):
     def __getitem__(self, n):
         if isinstance(n, int):
@@ -12,7 +13,7 @@ class Fib(object):
                 start = 0
             a, b = 1, 1
             L = []
-            for x in range(stop)
+            for x in range(stop):
                 if x >= start:
                     L.append(a)
                     a, b = b, a + b
@@ -21,3 +22,17 @@ class Fib(object):
 
 f = Fib()
 print(f[2])
+
+
+# python的__getattr__()特殊方法，链式调用
+class Chain(object):
+    def __init__(self, path=''):
+        self.__path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self.__path, path))
+
+    def __str__(self):
+        return self.__path
+
+    __repr__ = __str__
