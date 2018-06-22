3194c0c3d8a097b7439bb7a93aaa2e916e89718a
CHOPPER JJ <chopper_jj@qq.com>
2018-06-22 18:11:45 +0800

    update

--- LearningProject/IO.py
+++ LearningProject/IO.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+# IO编程
 from datetime import datetime
 
 with open('test.txt', 'w') as f:
@@ -16,3 +16,34 @@ with open('test.txt', 'rb') as f:
     s = f.read()
     print('open as binary for read...')
     print(s)
+
+# write to StringIO
+from io import StringIO
+
+f = StringIO()
+f.write('hello')
+f.write(' ')
+f.write('world!')
+print(f.getvalue())
+
+
+# read to stringIO
+f = StringIO('Helloooo!\nHi\nGoodbye!')
+while True:
+    s = f.readline()
+    if s == '':
+        break
+print(s.strip())
+
+
+# write to BytesIO
+from io import BytesIO
+
+b = BytesIO()
+b.write('中文'.encode('utf-8'))
+print(b.getvalue())
+
+# read to BytesIO
+date = '两情若是久长时，又岂在朝朝暮暮。'.encode('utf-8')
+bi = BytesIO(date)
+print(bi.read())
--- LearningProject/StringIO.py
+++ LearningProject/StringIO.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from io import StringIO
 
-f = StringIO
+f = StringIO()
 f.write('hello')
-f.write('')
+f.write(' ')
 f.write('world!')
-
 print(f.getvalue())
