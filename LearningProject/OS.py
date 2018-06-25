259c6158de7b9221ee04135160a9ef2e796409ed
CHOPPER JJ <chopper_jj@qq.com>
2018-06-25 18:05:04 +0800

    update

--- LearningProject/OS.py
+++ LearningProject/OS.py
@@ -12,4 +12,4 @@ for f in os.listdir(pwd):
     fsize = os.path.getsize(f)
     mtime = datetime.fromtimestamp(os.path.getatime(f)).strftime('%Y %m %d %H:%M')
     flag = '/' if os.path.isdir(f)else ''
-    print('%10d' % (fsize))
+    print('%10d %s %s%s' % (fsize, mtime, f, flag))
