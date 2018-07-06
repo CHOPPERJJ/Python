7f79003404fe08153ab4055e4ed3f1d0c3ce5730
CHOPPER JJ <chopper_jj@qq.com>
2018-07-06 18:04:33 +0800

    mysql py

--- LearningProject/do_PyMySQL.py
+++ LearningProject/do_PyMySQL.py
@@ -2,18 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import pymysql
+# 打开数据库连接
+db = pymysql.connect(host='localhost', user='root', password='123456', db='chopper', port='3306')
 
-db = pymysql.connect('localhost', 'user', '123456', 'TESTDB')
-cursor = db.cursor()
-cursor.execute('DROP TABLE IF EXISTS EMPLOYEE')
+# 使用cursor()方法获取操作游标
+cur = db.cursor()
 
-sql = """CREAT TABLE EMPLOYEE (
-         FIRST_NAME CHAR(20) NOT NULL,
-         LAST_NAME CHAR(20),
-         AGE INT,
-         SEX CHAR(1),
-         INCOME FLOAT )"""
+# 1.查询操作
+# 编写sql 查询语句 user 对应的表名
+sql = 'select * from user'
+try:
+    # 执行sql语句
+    cur.execute(sql)
+    # 获取查询的所有记录
+    result = cur.fetchall()
+    print('id', 'name', 'password')
 
-cursor.execute(sql)
+    # 遍历结果
+    for row in result:
+        id = row[0]
+        name = row[1]
+        password = row[2]
+        print(id, name, password)
 
-db.close()
+except Exception as e:
+    raise e
+finally:
+    db.close()
