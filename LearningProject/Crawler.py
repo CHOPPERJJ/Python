5c4d4176141ea3983ffad6b264d2c0eccafe51b9
CHOPPER JJ <chopper_jj@qq.com>
2018-07-09 00:46:46 +0800

    pachong

---
+++ LearningProject/Crawler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+
+
+from bs4 import BeautifulSoup
+# 传入URL
+r = requests.get('https://www.csdn.net/')
+
+# 解析URL
+soup = BeautifulSoup(r.text, 'html.parser')
+content_list = soup.find_all('div', attrs = {'class': 'title'})
+
+for content in content_list:
+    print(content.h2.a.text)
