d03c74368ab93468a35fe159e2de88c845733446
CHOPPER JJ <chopper_jj@qq.com>
2018-07-23 18:10:38 +0800

    mo.group(1)

--- LearningProject/WorkingProject/ChangeDateStyle.py
+++ LearningProject/WorkingProject/ChangeDateStyle.py
@@ -2,27 +2,38 @@
 # -*- coding: utf-8 -*-
 # MM-DD-YY美国时间表示转变为DD-MM-YY欧洲时间表示
 
-import shutil. os, re
+import os, re, shutil
 
 # 创建美国风格的正则表达式
 datePattern = re.compile(r'''^(.*?)
     ((0|1)?\d)-
-    ((0|1|2|3?\d)-
+    ((0|1|2|3)?\d)-
     ((19|20)\d\d)-
     (.*?)$
     ''', re.VERBOSE)
 
 # 遍历循环文件，寻找满足正则表达式的日期
-for amerFilename in os.listdir('.'):
+path = 'c:\\am_date'
+for amerFilename in os.listdir(path):
     mo = datePattern.search(amerFilename)
 
-# 略过没有时间的文本
-    if mo == none:
+    # 略过没有时间的文本
+    if mo == None:
         continue
 
 # 获取日期不同的组成部分
-brforePart = mo.group(1)
-monthPart  = mo.group(2)
-dayPart    = mo.group(4)
-yearPart   = mo.group(6)
-afterPart  = mo.group(8)
+beforePart = mo.group(1)
+monthPart = mo.group(2)
+dayPart = mo.group(4)
+yearPart = mo.group(6)
+afterPart = mo.group(8)
+
+# 转变为欧洲时间格式
+euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+absWorkingDir = os.path.abspath('am_date.txt')
+amerFilename = os.path.join(absWorkingDir, amerFilename)
+euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+# 重命名文件
+print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+# shutil.move(amerFilename, euroFilename)
