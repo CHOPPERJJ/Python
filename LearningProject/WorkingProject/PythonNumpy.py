7e9b1ced8b32af1739ec0b398d373b6ddc7d37fc
CHOPPER JJ <chopper_jj@qq.com>
2018-08-10 18:17:37 +0800

    apply

--- LearningProject/WorkingProject/PythonNumpy.py
+++ LearningProject/WorkingProject/PythonNumpy.py
@@ -3,6 +3,14 @@
 # Numerical Python高性能科学计算和数据分析基础包
 
 import numpy as np
-data1 = [6, 7.5, 8, 0, 1]
-arr1 = np.array(data1)
-print(arr1)
+
+# data1 = [6, 7.5, 8, 0, 1]
+# arr1 = np.array(data1)
+# print(arr1)
+
+# numpy的transpose函数的使用
+arr = np.arange(16).reshape((2, 2, 4))
+print(arr)
+
+arr2 = arr.transpose((1, 0, 2))
+print(arr2)
