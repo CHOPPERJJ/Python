fff0d48f09a9f633a93f9afb7d1f59051369bf3b
CHOPPER JJ <chopper_jj@qq.com>
2018-09-04 18:08:08 +0800

    python web django

--- LearningProject/WorkingProject/Temp.py
+++ LearningProject/WorkingProject/Temp.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import zipfile, os, shutil, re
+import redis
 
 # 爱心输出
 # print('\n'.join([''.join([('python!'[(x-y)%7]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
-
-
-mygenerator = (x*x for x in range(3))
-for i in mygenerator:
-    print(i)
+#
+#
+# mygenerator = (x*x for x in range(3))
+# for i in mygenerator:
+#     print(i)
