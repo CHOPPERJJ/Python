30ceab2e1e8c610b7a5657b58a1dc219e65003c4
CHOPPER JJ <chopper_jj@qq.com>
2018-09-12 18:15:16 +0800

    update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,12 +2,16 @@
 
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
