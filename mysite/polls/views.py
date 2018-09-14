e4fd28ca1678d73c745b5c95db5e7478e82fa0cd
CHOPPER JJ <chopper_jj@qq.com>
2018-09-14 18:03:03 +0800

    update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,8 +25,8 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-    response = "You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -35,7 +35,7 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoNotExist):
         return render(request, 'polls/detail.html', {
-            'question':question,
+            'question': question,
             'error_message': "You didn't select a choice."
         })
     else:
