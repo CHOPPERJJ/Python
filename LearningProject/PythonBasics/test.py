b4edc24000c1d3599c1d3114a2f5c54acf586fb9
CHOPPER JJ <chopper_jj@qq.com>
2018-09-18 18:07:11 +0800

    update 5

--- LearningProject/PythonBasics/test.py
+++ LearningProject/PythonBasics/test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import datetime
+from django.utils import timezone
 
-import Mysqldb
-print(Mysqldb)
+now = datetime.datetime.now()
+nextNow = now + datetime.timedelta(hours=23, minutes=59)
+nextNow2 = now + datetime.timedelta(days=1)
+print(now)
+print(nextNow)
+print(nextNow2)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
 
 
 # def index(request):
@@ -44,8 +45,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """return the last five published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        return the last five published questions (not including those set to be
+        published in the future).
+        """
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -66,7 +71,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
+            'error_message': "You didn't select a choice, please selecet again!",
         })
     else:
         selected_choice.votes += 1
