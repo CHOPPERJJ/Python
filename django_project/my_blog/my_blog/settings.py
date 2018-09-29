9b4525708ab4e618e877b2576d89f58850367abb
CHOPPER JJ <chopper_jj@qq.com>
2018-09-29 18:08:27 +0800

    my_blog

--- django_project/my_blog/my_blog/settings.py
+++ django_project/my_blog/my_blog/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 定义模板位置
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
