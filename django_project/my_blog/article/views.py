80952e72e19520e1270bf2bd83debbdc2f8a3e66
CHOPPER JJ <chopper_jj@qq.com>
2018-10-10 17:58:59 +0800

    update

--- django_project/my_blog/article/views.py
+++ django_project/my_blog/article/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import markdown
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -10,3 +11,15 @@ def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+        extensions = [
+        'markdown.extensions.extra',
+        'markdown.extension.codehilite'
+         ])
+
+    context = { 'article': article }
+    return render(request, 'article/detail.html', context)
