b2500ac410be8719b9439ee312f26255c301d370
CHOPPER JJ <chopper_jj@qq.com>
2018-10-23 18:09:18 +0800

    django_blog

--- django_project/mysite/mysite/settings.py
+++ django_project/mysite/mysite/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chopperdb',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306'
+
     }
 }
 
