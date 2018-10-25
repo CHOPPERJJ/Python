96d86ff51829636aa44ec241dcc0129b2db34215
CHOPPER JJ <chopper_jj@qq.com>
2018-10-25 18:07:29 +0800

    django pymysql

--- django_project/mysite/mysite/settings.py
+++ django_project/mysite/mysite/settings.py
@@ -78,11 +78,14 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chopperdb',
+        'NAME': 'chopper',
         'USER': 'root',
         'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '3306'
+        'OPTIONS': {
+            'autocommit': Ture
+        },
 
     }
 }
